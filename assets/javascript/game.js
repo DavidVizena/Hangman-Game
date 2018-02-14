52544146d4270a34319408a383e070db82280067
Vizena2005 <vizena2005@gmail.com>
2018-02-14 11:10:52 -0600

    attempting to fix the underscore problem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,7 @@ var remainingLives = 5;
 
 // Dom manipulation
 var docUnderScore = document.getElementsByClassName('underscore');
+var docNewUnderScore = document.getElementsByClassName('underscore');
 var docCorrectGuess = document.getElementsByClassName('correctGuess');
 var docincorrectGuess = document.getElementsByClassName('incorrectGuess');
 var docwinCondition = document.getElementsByClassName('wins');
@@ -23,14 +24,28 @@ var docSubtractLife = document.getElementsByClassName('lives');
 // =========================================================
 console.log(choosenWord);
 // Creates underscores based on word length
- generateUnderscore = () => {
+generateUnderscore = () => {
     for (let i = 0; i < choosenWord.length; i++){
         underScore.push('_');
     }
     return underScore;
 }
+// prints the number of underscores to the page
+docUnderScore[0].innerHTML = generateUnderscore().join(' ');
+// function to reset the lives/guess boxes
+function reset(){
+    remainingLives = 5;
+    docSubtractLife[0].innerHTML = "Lives Left: 5";
+    docCorrectGuess[0].innerHTML = "Correct Guess: ";
+    docincorrectGuess[0].innerHTML = "Incorrect Guess: ";
+    docNewUnderScore.innerHTML = generateUnderscore().join(' ');
+    generateUnderscore();
+    docUnderScore[0].innerHTML = generateUnderscore().join(' ');
+    var randNum = Math.floor(Math.random() * words.length);
+    var choosenWord = words[randNum];
+}
 // Aquires users guess
-document.addEventListener('keypress', (event) => {
+    document.addEventListener('keypress', (event) => {
     let keyword = String.fromCharCode(event.keyCode);
 // If users guess is correct
     if(choosenWord.indexOf(keyword) > -1) {
@@ -42,7 +57,6 @@ document.addEventListener('keypress', (event) => {
         docCorrectGuess[0].innerHTML = "Correct Gueses:" + correctWord;
 // Checks to see if user word matches guesses
         if(underScore.join('') == choosenWord) {
-        // Generates a new word if the user gets the word correct
     }
   }
 //   if word is incorrect it will take a life away
@@ -50,19 +64,27 @@ document.addEventListener('keypress', (event) => {
     incorrectWord.push(keyword);
     remainingLives--;
     docSubtractLife[0].innerHTML = "Lives left: " + remainingLives;
-    docincorrectGuess[0].innerHTML = incorrectWord;
+    docincorrectGuess[0].innerHTML = "Incorrect Guesses: " + incorrectWord;
   }
 //   when all lives are spent you lose the game
   if(remainingLives === 0){
-      loss++;
-      remainingLives = 5;
-        docLossCondition[0].innerHTML = "Losses: " + loss;
+    // adds a loss to the loss box  
+    loss++;
+    // resets the lives and guesses after a loss
+    reset();
+    // generates new word after a loss
+      docLossCondition[0].innerHTML = "Losses: " + loss;
   }
-  if(correctWord.join('') === choosenWord){
+//   else if to add a win if you guess the word
+  else if(correctWord.join('') === choosenWord){
+    // resets the lives and guesses
+    reset();
+    // adds a win to the win box
     win++;
+    // makes a new word to guess
     docwinCondition[0].innerHTML = "Wins: " + win;
+
 }
 });
-docUnderScore[0].innerHTML = generateUnderscore().join(' ');
 
 
